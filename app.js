88285e14fa8779ea1fd95b78d377e9073ebf63de
LoayIssa <issaloay680@gmail.com>
2021-05-16 13:32:42 +0300

    solved pulse check q3..q4..q1 in practic

--- app.js
+++ app.js
@@ -5,10 +5,31 @@ const db = require("./db");
 const app = express();
 app.use(express.json());
 
-app.get("/todos", (req, res) => {});
-app.post("/create/todo", (req, res) => {});
-app.put("/update/todo", (req, res) => {});
-app.delete("/delete/todo", (req, res) => {});
+app.post("/create/todo", (req, res) => {
+  const {task , description , deadline , isCompleted , priority } = req.body;
+  const todo = new todoModel (
+
+    {task , description , deadline , isCompleted , priority }
+
+  )
+
+ todo.save().then(result=>{res.json(result)}).catch(err=>{res.send(err)})
+
+});
+app.get("/todos", (req, res) => {
+    todoModel.find().then((result)=>{
+        res.json(result);
+    }).catch((err)=>{res.json(err)})
+
+});
+
+
+
+
+
+ 
+// app.put("/update/todo", (req, res) => {});
+// app.delete("/delete/todo", (req, res) => {});
 
 const port = 3000;
 app.listen(port, () => {
--- schema.js
+++ schema.js
@@ -1,1 +1,10 @@
 const mongoose = require("mongoose");
+const todoSchema = new mongoose.Schema({
+    task: { type: String, required: true },
+    description: { type: String },
+    deadline: { type: String },
+    isCompleted: { type: Boolean },
+    priority: { type: String },
+  });
+
+  module.exports = mongoose.model("todo ",todoSchema)
